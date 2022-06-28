a62998695c3261d120f1e5af078f530ef9079ec4
ntduncan <nateduncan0@.com>
2022-06-27 22:14:37 -0600

    completed api with one endpoint

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BooksApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
